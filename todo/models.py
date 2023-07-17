7b1c22d54bf6258910cbc684ff2a316c2771f905
DanilKurochkin <78516434+DanilKurochkin@users.noreply.github.com>
2023-07-17 20:25:12 +0300

    Log in

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.core.validators import MaxLengthValidator
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=80, validators=[MaxLengthValidator])
+    memo = models.TextField(blank=True,max_length=500, validators=[MaxLengthValidator])
+    is_important = models.BooleanField()
--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def home(request):
     return render(request, 'todo/home.html')
 
+def redirecttohome(request):
+    return redirect('home')
+
 def signupuser(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            user = User.objects.create_user(username=request.POST['username'],
+                                            password=request.POST['password'])
             user.save()
             login(request, user)
             return redirect('todocurrent')
@@ -27,3 +32,19 @@ def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+def loginuser(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        form.is_valid()
+        user = authenticate(request, username=request.POST['username'],
+                            password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/login.html', 
+                          {'form':form,
+                           'error':'Incorrect username or password'})
+        login(request, user)
+        return redirect('todocurrent')
+    else: 
+        form = AuthenticationForm()
+    return render(request, 'todo/login.html', {'form':form})
